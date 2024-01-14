b826aa6c759c2d32648dde80b0016c8ead9e30d8
Stanley643 <nmurigi@kabarak.ac.ke>
2024-01-14 22:29:03 +0300

    code a code

--- corruption_reporting_site/reports/urls.py
+++ corruption_reporting_site/reports/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ReportListCreate
+from .views import ReportListCreate, ReportListView, ReportDetailView, ReportCreateView
 
 urlpatterns = [
     path('api/reports/', ReportListCreate.as_view(), name='report-list-create'),
+    path('reports/', ReportListView.as_view(), name='report-list'),
+    path('reports/int:pk/', ReportDetailView.as_view(), name='report-detail'),
+    path('reports/new/', ReportCreateView.as_view(), name='report-create'),
 ]
--- corruption_reporting_site/reports/views.py
+++ corruption_reporting_site/reports/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
 
 # Create your views here.
 from rest_framework import generics
@@ -8,3 +9,20 @@ from .serializers import ReportSerializer
 class ReportListCreate(generics.ListCreateAPIView):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
+class ReportListView(ListView):
+    model = Report
+    template_name = 'reports/report_list.html'
+    context_object_name = 'reports'
+
+class ReportDetailView(DetailView):
+    model = Report
+    template_name = 'reports/report_detail.html'
+
+class ReportCreateView(CreateView):
+    model = Report
+    template_name = 'reports/report_form.html'
+    fields = ['title', 'description', 'evidence_file']
+
+    def form_valid(self, form):
+        form.instance.reporter = self.request.user
+        return super().form_valid(form)
