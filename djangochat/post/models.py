417e4f45a44283535f4098cecd3fa98a0c9de0ec
Stanley643 <nmurigi@kabarak.ac.ke>
2024-02-15 23:14:28 +0300

    code a code

--- djangochat/post/models.py
+++ djangochat/post/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
-# Create your models here.
-class Register(models.Model):
-    name = models.CharField(max_length=100)
+class Registration(models.Model):
+    username = models.CharField(max_length=100, unique=True)
     email = models.EmailField(unique=True)
-    password = models
+    password = models.CharField(max_length=128)  # Length to accommodate hashed password
+
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password)
+        super(Registration, self).save(*args, **kwargs)
+
