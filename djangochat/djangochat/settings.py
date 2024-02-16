bac34df354c0b4d89c261991fce7fac9cc550af7
Stanley643 <nmurigi@kabarak.ac.ke>
2024-02-16 23:08:08 +0300

    code a code

--- djangochat/djangochat/settings.py
+++ djangochat/djangochat/settings.py
@@ -81,6 +81,9 @@ DATABASES = {
     }
 }
 
+#AUTH_USER_MODEL = 'post.Registration'
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- djangochat/post/models.py
+++ djangochat/post/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password
+#from django.conf import settings
 
 class Registration(models.Model):
     username = models.CharField(max_length=100, unique=True)
@@ -10,3 +11,10 @@ class Registration(models.Model):
         self.password = make_password(self.password)
         super(Registration, self).save(*args, **kwargs)
 
+class Report(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    reporter = models.ForeignKey(Registration, on_delete=models.CASCADE)
+    evidence_file = models.FileField(upload_to='evidences/')
