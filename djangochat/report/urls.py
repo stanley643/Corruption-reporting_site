de7da092875854ece663375c2777a701fdf802d3
Stanley643 <nmurigi@kabarak.ac.ke>
2024-02-22 15:12:05 +0300

    code a code

---
+++ djangochat/report/urls.py
@@ -0,0 +1,11 @@
+# urls.py
+
+from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView
+
+urlpatterns = [
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/create/', PostCreateView.as_view(), name='post-create'),
+    # Add more URL patterns as needed
+]
--- djangochat/report/views.py
+++ djangochat/report/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# views.py
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post
+from .forms import PostForm
+
+# View for displaying a list of posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'  # Replace with your template name
+    context_object_name = 'posts'
+
+# View for displaying details of a post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'  # Replace with your template name
+    context_object_name = 'post'
+
+# View for creating a new post
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm  # Replace with your form class if you have one
+    template_name = 'post_form.html'  # Replace with your template name
+
+    def form_valid(self, form):
+        # Perform additional actions if needed before saving the form
+        return super().form_valid(form)
