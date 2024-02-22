e2259a2919feb4e88cf8fdda9b43ed15084492c7
Stanley643 <nmurigi@kabarak.ac.ke>
2024-02-22 12:56:18 +0300

    code a code

--- djangochat/post/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('report/', views.create_report, name='create_report')
-
-]
