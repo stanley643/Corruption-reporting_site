9f9c84d1d8b306cef8c65b2cf582eac6726d7119
Stanley643 <nmurigi@kabarak.ac.ke>
2024-02-22 12:17:40 +0300

    code a code

--- corruption_reporting_site/reports/admin.py
+++ corruption_reporting_site/reports/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Report, UserProfile, ChatMessage, CustomUser
+from .models import Report, UserProfile, Message, CustomUser
 
 
 admin.site.register(Report)
 admin.site.register(UserProfile)
-admin.site.register(ChatMessage)
+admin.site.register(Message)
 admin.site.register(CustomUser)
