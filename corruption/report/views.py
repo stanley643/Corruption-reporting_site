ec3a13e0c8b104d992608a515572272f79b0c653
Stanley643 <nmurigi@kabarak.ac.ke>
2024-03-05 00:04:17 +0300

    code a code

--- corruption/report/views.py
+++ corruption/report/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegistrationForm
+from .models import Post
 
 def register(request):
     if request.method == 'POST':
@@ -14,3 +16,13 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'report/register.html', {'form': form})
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'report/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'report/post_detail.html', {'post': post})
+
+
