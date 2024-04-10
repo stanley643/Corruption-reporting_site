f490219a273671b5c7730352ea8b220dec6a28a1
vincent <vincentbettoh@gmail.com>
2024-04-10 14:40:46 -0400

    pip install requirements inside django chat

--- djangochat/djangochat/urls.py
+++ djangochat/djangochat/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from djangochat import views as my_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
